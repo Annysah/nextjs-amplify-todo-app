4f09484685b6312a381d12cb2b2320ac6caefc9c
Anisat Akinbani <52763841+Annysah@users.noreply.github.com>
2023-01-14 18:05:30 -0600

    update this repo

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+//import config from "../aws-exports";
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,56 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React, { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Amplify, API } from "aws-amplify";
+import awsconfig from "../src/aws-exports";
 
-export default function Home() {
+import CreateTodo from "../components/CreateTodo";
+import Todo from "../components/Todos";
+
+import * as mutations from "../src/graphql/mutations";
+import * as queries from "../src/graphql/queries";
+
+Amplify.configure(awsconfig);
+
+export default function Home({ todos }) {
+  const [todoList, setTodoList] = useState(todos);
+
+  const onCreateTodo = async (todo) => {
+    const newTodo = {
+      title: todo,
+      completed: false,
+    };
+
+    try {
+      await API.graphql({
+        query: mutations.createTodo,
+        variables: { input: newTodo },
+      });
+
+      setTodoList((list) => [...list, { ...newTodo }]);
+
+      console.log("Successfully created a todo!");
+    } catch (err) {
+      console.log("error: ", err);
+    }
+  };
+
+  const onDeleteTodo = async (id) => {
+    try {
+      await API.graphql({
+        query: mutations.deleteTodo,
+        variables: { input: { id } },
+      });
+
+      const filterTodo = todoList.filter((todo) => todo.id !== id);
+      setTodoList(filterTodo);
+
+      console.log("Successfully deleted a todo!");
+    } catch (err) {
+      console.log({ err });
+    }
+  };
+
   return (
     <>
       <Head>
@@ -14,110 +59,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <CreateTodo onCreateTodo={onCreateTodo} />
+        <Todo todoList={todoList} onDeleteTodo={onDeleteTodo} />
       </main>
     </>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const todoData = await API.graphql({
+    query: queries.listTodos,
+  });
+
+  return {
+    props: {
+      todos: todoData.data.listTodos.items,
+    }
+  };
 }
